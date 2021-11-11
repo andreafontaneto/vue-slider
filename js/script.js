decfecb4391d49d8dccc04a36e4bff7ff256bf66
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-11 13:39:23 +0100

    init project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+
+Rifare l’esercizio dello slider come fatto assieme in classe.
+
+Bonus:
+Applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+*/
+
+console.log('ciao');
